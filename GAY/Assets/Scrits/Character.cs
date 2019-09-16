fb89ae50757140d8cf5f50814f969536e9553693
Mr.Ur1s <mr.ur1s@mail.ru>
2019-09-16 15:14:34 +0700

    создание персонажа v1

---
+++ GAY/Assets/Scrits/Character.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scrits
+{
+    class Character
+    {
+        string rase;
+        string clas;
+        int fiz_at, fiz_def, mag_at, mag_def, accuracy, evasion;
+
+        public Character(string rase,string clas,int fiz_at, int fiz_def, int mag_at, int mag_def, int accuracy, int evasion)
+        {
+            this.evasion = evasion;
+            this.accuracy = accuracy;
+            this.mag_def = mag_def;
+            this.mag_at = mag_at;
+            this.fiz_def = fiz_def;
+            this.fiz_at = fiz_at;
+            this.clas = clas;
+            this.rase = rase;
+        }
+
+        void add_characteristic(string characteristic, int add)
+        {
+            switch(characteristic)
+            {
+                case "fiz_at":
+                    fiz_at += add;
+                    break;
+                case "fiz_def":
+                    fiz_def += add;
+                    break;
+                case "mag_at":
+                    mag_at += add;
+                    break;
+                case "mag_def":
+                    mag_def += add;
+                    break;
+                case "accuracy":
+                    accuracy += add;
+                    break;
+                case "evasion":
+                    evasion += add;
+                    break;
+            }            
+        }
+    }
+}
---
+++ GAY/Assets/Scrits/Character_changer_color.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character_changer_color : MonoBehaviour {
+
+    public GameObject sphere;
+	// Update is called once per frame
+	void OnGUI()
+    {
+        
+    }
+}
