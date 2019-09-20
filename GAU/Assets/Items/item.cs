e354e3817348446db806ae51a473055142b1d5c8
Mr.Ur1s <mr.ur1s@mail.ru>
2019-09-20 18:54:41 +0700

    передвижение персонажа

--- GAU/Assets/Items/item.cs
+++ GAU/Assets/Items/item.cs
@@ -38,3 +38,24 @@ namespace Assets.Scrits
         }
     }
 }
+
+/*
+        string path = "Assets/Items/Items.xml";
+        XDocument xDoc = XDocument.Load(path);
+        XElement root = xDoc.Element("Items");
+        foreach (XElement xEle in root.Elements("id"))
+        {
+            id=Convert.ToInt32(xEle.Attribute("id").Value.Trim());
+            name = xEle.Element("name").Value.Trim();
+            type = xEle.Element("type").Value.Trim();
+            fiz_at = Convert.ToInt32(xEle.Element("fiz_at").Value.Trim());
+            fiz_def = Convert.ToInt32(xEle.Element("fiz_def").Value.Trim());
+            mag_at = Convert.ToInt32(xEle.Element("mag_at").Value.Trim());
+            mag_def = Convert.ToInt32(xEle.Element("mag_def").Value.Trim());
+            accuracy = Convert.ToInt32(xEle.Element("accuracy").Value.Trim());
+            evasion = Convert.ToInt32(xEle.Element("evasion").Value.Trim());
+            speed = Convert.ToInt32(xEle.Element("speed").Value.Trim());
+            isMelee = Convert.ToBoolean(xEle.Element("isMelee").Value.Trim());
+            description = xEle.Element("description").Value.Trim();
+        }
+ */
--- GAU/Assets/Scrits/Test.cs
+++ GAU/Assets/Scrits/Test.cs
@@ -7,57 +7,32 @@ using System;
 
 public class Test : MonoBehaviour
 {
-    // Start is called before the first frame update
-
-    /*public string rase;
-    public string clas;
-    public int fiz_at = 0,
-        fiz_def = 0,
-        mag_at = 0,
-        mag_def = 0,
-        accuracy = 0,
-        evasion = 0,
-        speed = 0;
-    public bool isMelee;
-    */
-    public int id;
-    public string name;
-    public string type;
-    public bool isMelee;
-    public int fiz_at = 0,
-        fiz_def = 0,
-        mag_at = 0,
-        mag_def = 0,
-        accuracy = 0,
-        evasion = 0,
-        speed = 0;
-    public string description;
+    public GameObject player;
+    Vector3 target;
+    public float speed = 0.5f;
+    public bool isMove = true;
+    public bool isClick = false;
     void Start()
     {
-        string path = "Assets/Items/Items.xml";
-        XDocument xDoc = XDocument.Load(path);
-        XElement root = xDoc.Element("Items");
-        foreach (XElement xEle in root.Elements("id"))
-        {
-            id=Convert.ToInt32(xEle.Attribute("id").Value.Trim());
-            name = xEle.Element("name").Value.Trim();
-            type = xEle.Element("type").Value.Trim();
-            fiz_at = Convert.ToInt32(xEle.Element("fiz_at").Value.Trim());
-            fiz_def = Convert.ToInt32(xEle.Element("fiz_def").Value.Trim());
-            mag_at = Convert.ToInt32(xEle.Element("mag_at").Value.Trim());
-            mag_def = Convert.ToInt32(xEle.Element("mag_def").Value.Trim());
-            accuracy = Convert.ToInt32(xEle.Element("accuracy").Value.Trim());
-            evasion = Convert.ToInt32(xEle.Element("evasion").Value.Trim());
-            speed = Convert.ToInt32(xEle.Element("speed").Value.Trim());
-            isMelee = Convert.ToBoolean(xEle.Element("isMelee").Value.Trim());
-            description = xEle.Element("description").Value.Trim();
-        }
-        
+        target = player.GetComponent<Transform>().localPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+            target = Input.mousePosition; 
+
+        if (target != player.GetComponent<Transform>().localPosition)
+        {
+             Vector3 move = (target - player.GetComponent<Transform>().localPosition);
+            player.GetComponent<Transform>().localPosition += move * Time.deltaTime;
+            
+        } 
     }
+    public void click()
+    {
+        isClick = true;
+    }
+
 }
