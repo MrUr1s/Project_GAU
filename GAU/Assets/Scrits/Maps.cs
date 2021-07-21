c2b87039399afbe8011feaeb2052bcc28ac9c1df
MrUr1s <mr.ur1s@mail.ru>
2021-07-21 13:23:45 +0700

    начало в2

---
+++ GAU/Assets/Scrits/Maps.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class write : MonoBehaviour
+{
+    public GameObject level;
+    // Start is called before the first frame updatet;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Escape))
+        Delete();
+    }
+
+
+    public Vector2 getRandomSize(float size)
+    {
+        float rnd=Random.Range(1,10);
+        float x=2*Mathf.PI*rnd;
+        float y = rnd + Random.Range(10, 20);
+        return new Vector2(size * x, size * y);
+    }
+
+    public Vector3 getRandomPos(float radius)
+    {
+        float rnd = Random.Range(1, 10);
+        float x = Mathf.PI * rnd*Mathf.Cos(radius);
+        float y = Mathf.PI * rnd*Mathf.Sin(radius);
+
+        return new Vector3(radius * x, radius * y, 0);
+    }
+    public void Generation(int y)
+    {
+        for (int i = 0; i < y; i++)
+        {
+            GameObject go = Instantiate(level,getRandomPos(Random.Range(1,10))+ new Vector3(400, 240, 0), Quaternion.identity, GetComponent<Transform>());
+            go.GetComponent<BoxCollider2D>().size = go.GetComponent<RectTransform>().sizeDelta = getRandomSize(10);
+            go.name = "ID " + i.ToString();
+        }
+
+    }
+    public void Delete()
+    {
+        foreach (var go in GameObject.FindGameObjectsWithTag("Level"))
+            Destroy(go);
+      
+    }
+}
---
+++ GAU/Assets/Scrits/item.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scrits
+{
+    class item
+    {
+        public string name;
+        public int id;
+        public bool isMelee;
+        public int fiz_at = 0,
+            fiz_def = 0,
+            mag_at = 0,
+            mag_def = 0,
+            accuracy = 0,
+            evasion = 0,
+            speed = 0;
+        public string description;
+
+        public item(string name,int id, bool isMelee, int fiz_at, int fiz_def, int mag_at, int mag_def, int accuracy, int evasion, int speed, string description)
+        {
+            this.id = id;
+            this.name = name;
+            this.isMelee = isMelee;
+            this.fiz_at = fiz_at;
+            this.fiz_def = fiz_def;
+            this.mag_at = mag_at;
+            this.mag_def = mag_def;
+            this.accuracy = accuracy;
+            this.evasion = evasion;
+            this.speed = speed;
+            this.description = description;
+        }
+    }
+}
